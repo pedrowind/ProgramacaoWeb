56a1e062fe8acea41eb257cfff30cc00f56db3e0
Pedro Windmoller <142617499+pedrowind@users.noreply.github.com>
2024-11-01 19:58:22 -0300

    Add files via upload

---
+++ Back-End/ExemploWebAPIPOO/ExemploWebAPIPOO/Controllers/PessoaController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExemploWebAPIPOO.Controllers
+{
+    public class PessoaController : ControllerBase
+    {
+        [HttpPost("InserirPessoa")]
+        public string InserirPessoa(string cpfPessoa, string nomePessoa, 
+                                    DateTime dataNascimento, string telefone, 
+                                    string nomeMaePessoa, string emailPessoa)
+        {
+            return "Registro inserido com sucesso.";
+        }
+        [HttpPut("AlterarPessoa")]
+        public string AlterarPessoa(string cpfPessoa, string nomePessoa,
+                                    DateTime dataNascimento, string telefone,
+                                    string nomeMaePessoa, string emailPessoa)
+        {
+            return "Registro alterado com sucesso.";
+        }
+        [HttpDelete("ExcluirPessoa")]
+        public string ExcluirPessoa(string cpfPessoa, string nomePessoa,
+                                    DateTime dataNascimento, string telefone,
+                                    string nomeMaePessoa, string emailPessoa)
+        {
+            return "Registro excluido com sucesso.";
+        }
+        [HttpGet("PesquisarTodos")]
+        public string PesquisarTodos(string cpfPessoa, string nomePessoa,
+                                    DateTime dataNascimento, string telefone,
+                                    string nomeMaePessoa, string emailPessoa)
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.cpf = cpfPessoa;
+            pessoa.nome = nomePessoa;
+            pessoa.nomeMae = nomeMaePessoa; 
+            pessoa.dataNascimento = dataNascimento;
+            pessoa.email = emailPessoa;
+            pessoa.telefone = telefone;
+
+            return pessoa.PesquisarTodos();
+        }
+    }
+}
---
+++ Back-End/ExemploWebAPIPOO/ExemploWebAPIPOO/Dominio/Pessoa.cs
@@ -0,0 +1,45 @@
+﻿namespace ExemploWebAPIPOO
+{
+    public class Pessoa
+    {
+        //Atributos
+        public string cpf;
+        public string nome;
+        public DateTime dataNascimento;
+        public string nomeMae;
+        public string telefone;
+        public string email;
+
+        //Metodos
+        public void Inserir()
+        {
+            //Incluir pessoa
+        }
+        public string PesquisarTodos()
+        {
+            string retorno = $"CPF: {cpf}\r\n" +
+                             $"Nome: {nome}\r\n" +
+                             $"Nome da Mãe: {nomeMae}\r\n" +
+                             $"Telefone: {telefone}\r\n" +
+                             $"Email: {email}\r\n";
+            return retorno;
+        }
+        public string PesquisarPorCpf(string cpf)
+        {
+            string retorno = $"CPF: {cpf}\r\n" +
+                 $"Nome: {nome}\r\n" +
+                 $"Nome da Mãe: {nomeMae}\r\n" +
+                 $"Telefone: {telefone}\r\n" +
+                 $"Email: {email}\r\n";
+            return retorno;
+        }
+        public void Alterar(Int32 id)
+        {
+            //Incluir pessoa
+        }
+        public void Excluir(Int32 id)
+        {
+            //Incluir pessoa
+        }
+    }
+}
