2bacb4b791938c2af6fe40b4962a6744c0e1aa86
Pedro Windmoller <142617499+pedrowind@users.noreply.github.com>
2024-11-01 19:57:35 -0300

    Add files via upload

---
+++ Back-End/Exemplo4C#/Exemplo4C#/Program.cs
@@ -0,0 +1,16 @@
+﻿Int32 idade;
+String resultado;
+
+Console.WriteLine("Informe a idade: ");
+idade = Convert.ToInt32(Console.ReadLine());
+resultado = (idade > 17) ? "Pode dirigir." : "Não pode dirigir.";
+Console.WriteLine(resultado);
+
+//if (idade > 17)
+//{
+//    Console.WriteLine("Pode dirigir.");
+//}
+//else
+//{
+//    Console.WriteLine("Não pode dirigir.");
+//}
---
+++ Back-End/Exemplo5C#/Exemplo5C#/Program.cs
@@ -0,0 +1,42 @@
+﻿int numero;
+int[] pares = new int[10];
+int[] impares = new int[10];
+
+try {
+    for (int i = 1; i < 10; i++)
+    {
+        Console.WriteLine("Insira o numero:" + i);
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            pares[i] = numero;
+        }
+        else
+        {
+            impares[i] = numero;
+        }
+    }
+
+    for (int i = 0; i < pares.Length; i++)
+    {
+        if (pares[i] != 0) {
+            Console.WriteLine(pares[i]);
+        }
+
+    }
+
+    Console.WriteLine("=======================================");
+
+    for (int i = 0; i < impares.Length; i++)
+    {
+        if (impares[i] != 0)
+        {
+            Console.WriteLine(impares[i]);
+        }
+    }
+}
+
+catch (Exception ex) { 
+    Console.WriteLine("Digita certo ai pô");
+}
