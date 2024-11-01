ff3542540acba50a90316b172ed52f104d8bb681
Pedro Windmoller <142617499+pedrowind@users.noreply.github.com>
2024-11-01 19:54:50 -0300

    Add files via upload

---
+++ Back-End/Exemplo2C#/Exemplo2C#/Program.cs
@@ -0,0 +1,14 @@
+﻿double area, baseMenor, baseMaior, h;
+
+Console.WriteLine("Insira o valor da base maior: ");
+baseMaior = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Insira o valor da base menor: ");
+baseMenor = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Insira o valor da altura: ");
+h = Convert.ToDouble(Console.ReadLine());
+
+area = (baseMaior + baseMenor) * h / 2;
+
+Console.WriteLine($"A área do trapézio é: {area}");
