c0031fa3b804eeb6f86f697a0d8899f707472726
Pedro Windmoller <142617499+pedrowind@users.noreply.github.com>
2024-11-01 19:51:42 -0300

    Delete Exemplo1C# directory

--- Exemplo1C#/Exemplo1C#/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿string nome, saudacao;
-//Entrada
-Console.WriteLine("Digite seu nome: ");
-nome = Console.ReadLine();
-//Processamento
-saudacao = $"Seja bem-vindo {nome}";
-//Saída
-Console.WriteLine(saudacao);
