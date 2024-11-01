bde7a8220770ce2e93c0fbdf5b1dbf29efbb6d8f
Pedro Windmoller <142617499+pedrowind@users.noreply.github.com>
2024-11-01 19:47:54 -0300

    Add files via upload

---
+++ Exemplo1C#/Exemplo1C#/Program.cs
@@ -0,0 +1,8 @@
+﻿string nome, saudacao;
+//Entrada
+Console.WriteLine("Digite seu nome: ");
+nome = Console.ReadLine();
+//Processamento
+saudacao = $"Seja bem-vindo {nome}";
+//Saída
+Console.WriteLine(saudacao);
