7851af2760f6f05f9792df851fb87905af700ba6
Pedro Windmoller <142617499+pedrowind@users.noreply.github.com>
2024-11-29 21:45:24 -0300

    Add files via upload

---
+++ Back-End/Entities/ItemVenda.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorVendasAN.Entities
+{
+    [Table("tbItemVenda")]
+    public class ItemVenda
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+        [Key]
+        public Produto Produto { get; set; }
+        public int PedidoId { get; set; }
+        public Pedido Pedido { get; set; }
+
+        public int QuantidadeProduto { get; set; }
+
+        public double PrecoUnitario {  get; set; }
+    }
+}
---
+++ Back-End/Entities/Pedido.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorVendasAN.Entities
+{
+    [Table("tbPedido")]
+    public class Pedido
+    {
+        [Key]
+        public int Id { get; set; } 
+        public DateTime DataPedido { get; set; }
+        public int FuncionarioId { get; set; }
+        public Funcionario Funcionario { get; set; }
+
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+
+        public int ValorTotal { get; set; }
+
+        public ICollection<ItemVenda> ItensVenda { get; set; }
+
+    }
+}
